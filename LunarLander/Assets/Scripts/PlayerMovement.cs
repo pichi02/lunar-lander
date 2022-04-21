d535a53141603794e366a4ca0b70ddda99b925ab
pichi02 <lucas.pich02@gmail.com>
2022-04-21 17:48:11 -0300

    físicas lunares

--- LunarLander/Assets/Scripts/PlayerMovement.cs
+++ LunarLander/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody rb;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float gravityModifier;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
