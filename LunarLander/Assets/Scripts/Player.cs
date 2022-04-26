673edba58cfc5329a3d6e1657f698cd13e6cacd0
pichi02 <lucas.pich02@gmail.com>
2022-04-26 10:35:17 -0300

    agregado de score al jugador y cambio de nombre de script

--- LunarLander/Assets/Scripts/Player.cs
+++ LunarLander/Assets/Scripts/Player.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody rb;
     [SerializeField] float jumpSpeed;
     [SerializeField] float gravityModifier;
     [SerializeField] float speed;
+    private int score = 0;
 
+    public int GetScore() => score;
 
+    public void IncreaseScore()
+    {
+        score += 10;
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,33 +26,33 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetMouseButton(0))
         {
             rb.AddRelativeForce(Vector3.up * jumpSpeed);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             rb.AddRelativeTorque(Vector3.left * 0.1f);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             rb.AddRelativeTorque(Vector3.right * 0.1f);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             rb.AddRelativeTorque(Vector3.forward * 0.1f);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             rb.AddRelativeTorque(Vector3.back * 0.1f);
         }
-        //if (Input.GetKey(KeyCode.Q))
-        //{
-        //    rb.AddRelativeTorque(Vector3.down * 0.1f);
-        //}
-        //if (Input.GetKey(KeyCode.E))
-        //{
-        //    rb.AddRelativeTorque(Vector3.up * 0.1f);
-        //}
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rb.AddRelativeTorque(Vector3.down);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            rb.AddRelativeTorque(Vector3.up);
+        }
     }
 }
